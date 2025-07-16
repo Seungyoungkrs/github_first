046912ccb246047b732be4b23c615876203a3fed
Seungyoungkrs <sylee82@krs.co.kr>
2025-07-16 15:38:48 +0900

    feat: prompt user for nave before hello world

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GreetUser();
+    }
+
+    static void GreetUser()
+    {
+        Console.Write("당신의 이름을 입력하세요: ");
+        string name = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(nave))
+        {
+            Console.WriteLine("이름 입력 없음");
+        }
+        else
+        {
+            Console.WriteLine("이름 입력됨");
+        }
+        
+        Console.WriteLine($"현재 시각은 {DateTime.Now} 입니다.")
+    }
+}
